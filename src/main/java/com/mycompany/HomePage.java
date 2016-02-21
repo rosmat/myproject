45ae3cc6c7b4fc35e462df37b63306f64da26710
rosmat <rosmat@o2.pl>
2016-02-21 10:59:16 +0100

    add ProductPage

--- src/main/java/com/mycompany/HomePage.java
+++ src/main/java/com/mycompany/HomePage.java
@@ -1,18 +1,23 @@
 package com.mycompany;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public HomePage(final PageParameters parameters) {
-		super(parameters);
+    public HomePage(final PageParameters parameters) {
+        super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-		// TODO Add your page's components here
-
+        add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+        //TODO:lambda?
+        add(new Link("link1") {
+            @Override
+            public void onClick() {
+                setResponsePage(ProductPage.class);
+            }
+        });
     }
 }
---
+++ src/main/java/com/mycompany/ProductPage.java
@@ -0,0 +1,9 @@
+package com.mycompany;
+
+import org.apache.wicket.Page;
+
+/**
+ * Created by Mateusz on 17.02.2016.
+ */
+public class ProductPage extends Page {
+}
